5ee8535eb80c947c1e562b4da20579d8cc3b8a99
andresdanielmtz <autummata@gmail.com>
2025-04-22 20:14:36 -0600

    Remove: Node Modules, Dist. Add: Axios, Source code

--- dist/extension.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.analyzeCodeWithGemini = void 0;
-const analyzeCodeWithGemini = () => {
-    console.log(":D");
-};
-exports.analyzeCodeWithGemini = analyzeCodeWithGemini;
-console.log(":D");
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,28 @@
-export const analyzeCodeWithGemini = () => {
-  console.log(":D");
-};
+import axios from "axios";
 
-console.log(":D");
+export const analyzeCodeWithGemini = async (
+  code: string,
+  characters: { prompt: string; gif: string }
+) => {
+  const apikey = "AIzaSyCK77OqP6dHRFpbkcyjGRquqhdbHn3IOug";
+  if (!apikey) {
+    console.error("API key is missing");
+    return;
+  }
+  try {
+    const response = await axios.post(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
+        apikey,
+      {
+        contents: [
+          {
+            parts: [{ text: `${characters?.prompt}${code}` }],
+          },
+        ],
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    const result = response.data.candidates?.[0]?.content?.parts?.[0]?.text;
+    return result;
+  } catch (error) {}
+};
